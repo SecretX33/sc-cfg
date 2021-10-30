374edd0daea2c469d4ea61deb51362c4450f24c7
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2021-10-30 12:11:35 -0300

    Made Json serializer module use Configurate instead of standalone usage of Gson

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api(project(":api"))
     api("org.spongepowered:configurate-core:4.1.2")
     implementation("org.reflections:reflections:0.10.2")
-    compileOnlyApi("com.google.code.gson:gson:2.8.8")
+    compileOnly("com.google.code.gson:gson:2.8.8")
     compileOnly("com.google.guava:guava:31.0.1-jre")
     testImplementation("com.google.code.gson:gson:2.8.8")
     testImplementation("com.google.guava:guava:31.0.1-jre")
--- json/src/main/java/com/github/secretx33/sccfg/serialization/JsonSerializer.java
+++ json/src/main/java/com/github/secretx33/sccfg/serialization/JsonSerializer.java
@@ -15,70 +15,23 @@
  */
 package com.github.secretx33.sccfg.serialization;
 
-import com.github.secretx33.sccfg.wrapper.ConfigWrapper;
-import com.github.secretx33.sccfg.exception.ConfigDeserializationException;
-import com.github.secretx33.sccfg.exception.ConfigException;
-import com.github.secretx33.sccfg.exception.ConfigSerializationException;
 import com.github.secretx33.sccfg.serialization.gson.GsonFactory;
-import com.google.gson.Gson;
-import com.google.gson.JsonParseException;
+import org.spongepowered.configurate.gson.GsonConfigurationLoader;
+import org.spongepowered.configurate.loader.AbstractConfigurationLoader;
+import org.spongepowered.configurate.serialize.TypeSerializerCollection;
 
-import java.io.IOException;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public final class JsonSerializer extends AbstractSerializer {
+@SuppressWarnings("unused")
+public final class JsonSerializer extends AbstractConfigurateSerializer<GsonConfigurationLoader.Builder, GsonConfigurationLoader> {
 
     public JsonSerializer(final Logger logger, final GsonFactory gsonFactory) {
         super(logger, gsonFactory);
     }
 
     @Override
-    protected Map<String, Object> loadFromFile(ConfigWrapper<?> configWrapper) {
-        final Path path = configWrapper.getDestination();
-        final Gson gson = gsonFactory.getPrettyPrintInstance();
-        final String file;
-
-        try {
-            file = String.join("", Files.readAllLines(path, StandardCharsets.UTF_8));
-        } catch (final IOException e) {
-            logger.log(Level.SEVERE, "An IO error has occurred while reading your config file '" + configWrapper.getDestination().getFileName() + "' from the disk.", e);
-            return Collections.emptyMap();
-        }
-
-        try {
-            return gson.fromJson(file, linkedMapToken);
-        } catch (final JsonParseException e) {
-            logger.log(Level.SEVERE, "An error has occurred when deserializing file '" + configWrapper.getDestination().getFileName() + "' from " + configWrapper.getFileType() + ". There is probably some kind of typo on it, so it could not be parsed, please fix any typos on the file.", new ConfigDeserializationException(e));
-        }
-        return Collections.emptyMap();
-    }
-
-    @Override
-    protected void saveToFile(ConfigWrapper<?> configWrapper, Map<String, Object> newValues) {
-        final Path path = configWrapper.getDestination();
-        final Gson gson = gsonFactory.getPrettyPrintInstance();
-        final String file;
-
-        try {
-            file = gson.toJson(newValues, linkedMapToken);
-        } catch (final Exception e) {
-            final ConfigSerializationException ex = new ConfigSerializationException(e);
-            logger.log(Level.SEVERE, "An error has occurred when serializing config class " + configWrapper.getInstance().getClass().getName(), ex);
-            throw ex;
-        }
-
-        try (final Writer writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
-            writer.write(file);
-        } catch (final IOException e) {
-            logger.log(Level.SEVERE, "An error has occurred when saving your config file '" + configWrapper.getDestination().getFileName() + " to the disk.", e);
-            throw new ConfigException(e);
-        }
+    AbstractConfigurationLoader.Builder<GsonConfigurationLoader.Builder, GsonConfigurationLoader> fileBuilder() {
+        return GsonConfigurationLoader.builder()
+                .defaultOptions(opts -> opts.shouldCopyDefaults(false).serializers(TypeSerializerCollection.defaults()));
     }
 }
