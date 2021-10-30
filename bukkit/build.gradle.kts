38644f842ef238d13200b3b81f643ec42b235c10
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2021-10-30 12:12:26 -0300

    Switched to Jitpack version for MockBukkit, switched Gradle to 'bin' instead of 'all'

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,5 +1,6 @@
 repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
@@ -7,5 +8,5 @@ dependencies {
     implementation(project(":common"))
     compileOnly("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
     implementation("com.github.cryptomorin:XSeries:8.4.0")
-    testImplementation("com.github.seeseemelk:MockBukkit-v1.16:1.0.0")
+    testImplementation("com.github.seeseemelk:MockBukkit:v1.16-SNAPSHOT")
 }
